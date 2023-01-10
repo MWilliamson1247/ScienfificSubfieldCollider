aa7ab8fa761d6787a90f89e68d3177c53bcb3582
Dr. Mark R Williamson <Mark.Williamson.2@und.edu>
2023-01-10 13:59:26 -0600

    Add files via upload

---
+++ Idea_Collider.py
@@ -0,0 +1,24 @@
+
+#Part 1: Create List of Topics
+path="C:\\Users\\Mark.Williamson.2\\Desktop\\Williamson Data\\Various\\"
+file="ScienceDisciplines.txt"
+
+topic_list=[]
+for line in open(path+file):
+    if ' - ' in line:
+        topic=line.split(' - ')[0]
+    elif ' â€“ ' in line:
+        topic=line.split(' â€“ ')[0]
+    else:
+        topic=line.strip('\n')
+    topic_list.append(topic)    
+
+#Part 2: Function For Selecting 2 Random Topics
+def collider():
+    print("How many collisions do you want?: ")
+    x = int(input())
+    seq = [i+1 for i in range(x)]
+    for j in seq:
+        pair=random.sample(topic_list, 2)
+        print(pair[0] + ' + ' + pair[1]+ '\n')
+
